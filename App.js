84101d0ef559f6d1ef965bb340e6181528b27831
Vincent S. <vincent.silvestri@hotmail.fr>
2025-03-03 08:54:17 +0100

    initialiser React Navigation dans app.js

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
